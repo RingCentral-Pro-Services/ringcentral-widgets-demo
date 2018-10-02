40bbc6ee8ee7849fd16b2c745389e9f8c1985399
ekirshner <ekkicks@gmail.com>
2018-10-02 13:57:49 -0400

    multi window solution

--- extension/background.js
+++ extension/background.js
@@ -1,21 +1,28 @@
 
 let newWindow;
+let thereIsCurrentlyAWindowOpen = false;
 
 // pop small browser window for softphone
-chrome.browserAction.onClicked.addListener(function (tab) {
+chrome.browserAction.onClicked.addListener((tab) => {
+  if (!thereIsCurrentlyAWindowOpen) {
     chrome.windows.create({
       url: './standalong.html',
       type: 'popup',
       focused: true,
-      width: 300,
-      height: 536
-    }, function (wind) {
+      height: 536,
+      width: 300
+    }, (wind) => {
       newWindow = wind;
+      thereIsCurrentlyAWindowOpen = true;
     });
+  } else {
+    chrome.windows.update(newWindow.id, { focused: true });
+  }
 });
-chrome.windows.onRemoved.addListener(function (id) {
-  console.log(id);
+
+chrome.windows.onRemoved.addListener((id) => {
   if (newWindow && newWindow.id === id) {
     newWindow = null;
+    thereIsCurrentlyAWindowOpen = false;
   }
 });
